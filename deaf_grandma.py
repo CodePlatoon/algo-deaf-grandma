3a344cd31da0ba8900b488896783f37411b48a08
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-03-13 14:03:47 -0400

    created deaf grandmother app in for python.

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,38 @@
+from this import d
+
+
 def deaf_grandma():
+    goodbye = False
+    switch = 0
+    print("")
+    print("*************************")
+    print("Deaf Grandma: HEY KID!\n")
+    speak = input("Say Something: ")
 
-
+    if speak.upper() == speak:
+        if speak == "GOODBYE!":
+            print("")
+            print("Deaf Grandma: LEAVING SO SOON?\n")
+            speak = input("Say Something: ")
+            if speak == "GOODBYE!":
+                print("")
+                print("Deaf Grandma: LATER, SKATER!\n")
+                goodbye = True
+    if goodbye == True:
+        print("*** END OF PROGRAM ***")
+    else:
+        if speak == "":
+            print("")
+            print("Deaf Grandma: WHAT!?\n")
+            deaf_grandma()
+        elif speak.upper() == speak:
+            print("")
+            print("goodbye = ",goodbye)
+            print("Deaf Grandma: NO, NOT SINCE 1946\n")
+            deaf_grandma()
+        elif (speak.upper() != speak):
+            print("")
+            print("Deaf Grandma: SPEAK UP, KID!\n")
+            deaf_grandma()
 
 deaf_grandma()
