e288a88b245d11723e507a81de4bf5a578e13745
studentmdad <111543381+studentmdad@users.noreply.github.com>
2023-07-31 16:33:49 -0400

    Add files via upload

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,23 @@
-def deaf_grandma():
+import sys
+print("HEY KID!")
+goodbye_count=0
 
+while True:
+    response = input()
 
-
-deaf_grandma()
+    if response.isupper():
+        print("NO, NOT SINCE 1946!")
+    elif response == "":
+        print("WHAT?!")
+    else:
+        print("SPEAK UP, KID!")
+    
+    if response.upper()=="GOODBYE":
+        goodbye_count+=1
+        if goodbye_count==1:
+            print("LEAVING SO SOON?")
+            if response.isupper()=="GOODBYE":
+                goodbye_count+=1
+    if goodbye_count==2:
+        print("LATER, SKATER!")
+        sys.exit()
